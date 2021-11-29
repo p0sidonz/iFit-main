f5ae25c5c365eb96c421cd1db10dc8676d5bfe1c
p0sidonz <ethicalankitx0@gmail.com>
2021-11-29 21:25:52 +0530

    fixed the gql server endpoint

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -11,7 +11,7 @@ Vue.use(VueApollo)
 const AUTH_TOKEN = 'apollo-token'
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'http://127.0.0.1:8080/v1/graphql'
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://trainers.hasura.app/v1/graphql'
 // Files URL root
 export const filesRoot = process.env.VUE_APP_FILES_ROOT || httpEndpoint.substr(0, httpEndpoint.indexOf('/graphql'))
 
@@ -23,7 +23,7 @@ const defaultOptions = {
   httpEndpoint,
   // You can use `wss` for secure connection (recommended in production)
   // Use `null` to disable subscriptions
-  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'ws://127.0.0.1:8080/v1/graphql',
+  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'wss://trainers.hasura.app/v1/graphql',
   //wsEndpoint,
   // LocalStorage token
   tokenName: AUTH_TOKEN,
