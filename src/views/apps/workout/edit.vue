3d71f661554bb6fceb3730632373514c2857083a
p0sidonz <ethicalankitx0@gmail.com>
2021-12-23 17:42:05 +0530

    Update edit.vue

--- src/views/apps/workout/edit.vue
+++ src/views/apps/workout/edit.vue
@@ -458,8 +458,8 @@ export default {
       store
         .dispatch("app-workout/deleteExcercise", data.id)
         .then((response) => {
-          console.log(response)
-          if (response.data.data.delete_Fitness_workout_exercise_by_pk.id) {
+          console.log(response.data.data.delete_Fitness_workout_exercise_by_pk)
+          if (response.data.data.delete_Fitness_workout_exercise_by_pk) {
             this.isLoading = false;
             this.fetchExcercise();
             this.$toast({
